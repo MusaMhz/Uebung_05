814c3617ff9dcc6fd89b4755afed8f6cad8f18ff
Musawer Mohibzada <musawer@MacBook-Pro-von-Musawer.local>
2025-11-18 11:28:40 +0100

    H

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -1,5 +1,20 @@
 package p1;
 
 public class P1_main {
+	public static void main ( String [] args) {
+	
+		for (int x=1; x <= 11; x++) {
+
+		System.out.println(x);
+		}
+		
+		
+		int x= 0;
+		do {
+			x = x + 1;
+			System.out.println(x);
+		} while (x <= 10);
+}
+	
 
 }
--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -1,5 +1,21 @@
 package p2;
 
 public class P2_main {
+	public static void main ( String [] args) {
+		int[] numbers = {1,2,3,4,5};
+		int max = numbers[0];
+		int minIndex = 0; 
+		
+		for (int i = 1; i < numbers.length; i++) {
+			if (numbers[i] > max){
+				max = numbers[i];
+				minIndex = i;
+			}
+			
+		} 
+		System.out.println("max:" +max);
+		System.out.println("minIndex:" +minIndex);
+		
+	}
 
 }
